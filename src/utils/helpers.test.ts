9f2fc98ed9cc789754436248c8440004725057cc
John Isaiah Sangalang <esang037@gmail.com>
2023-08-06 15:13:50 +1200

    handle invalid sync byte

--- src/utils/helpers.test.ts
+++ src/utils/helpers.test.ts
@@ -1,7 +1,8 @@
-import { describe, expect, it, vi } from 'vitest';
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
 import { handleChunk, handleExtractPid, hasPid } from './helpers';
+import { PACKETSIZE } from './constants';
 
-describe('hasPid Function', () => {
+describe('hasPid Function', async () => {
   it('should return true if pid exists in the array', () => {
     const pids = [100, 200, 300];
     const pid = 200;
@@ -17,7 +18,7 @@ describe('hasPid Function', () => {
   });
 });
 
-describe('handleExtractPid Function', () => {
+describe('handleExtractPid Function', async () => {
   it('should extract and calculate PID correctly', () => {
     const packetBuffer = Buffer.from([0x47, 0x11, 0x22, 0x33]);
     const pid = handleExtractPid(packetBuffer);
@@ -28,27 +29,47 @@ describe('handleExtractPid Function', () => {
 
 describe('Testing handleChunk function', async () => {
   // Mock valid and invalid mock data
-  const validMockInputData = Buffer.from([
-    0x47, 0x10, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x47,
-    0x10, 0x20, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-  ]);
 
   const invalidMockInputData = Buffer.from([
     0x46, 0x10, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x47,
-    0x10, 0x20, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
   ]);
 
-  it('should handle errors', () => {
+  // Mock process.exit to capture exit calls
+  const originalProcessExit = process.exit;
+
+  let mockExitCode: number;
+  const mockExit = vi.fn((code?: number) => {
+    console.log(code);
+    mockExitCode = code || 0;
+  });
+  beforeEach(() => {
+    //@ts-ignore
+    process.exit = mockExit;
+  });
+
+  afterEach(() => {
+    process.exit = originalProcessExit;
+  });
+
+  // Mock console.log and console.error to capture log calls
+  const mockConsoleLog = vi.spyOn(console, 'log');
+  const mockConsoleError = vi.spyOn(console, 'error');
+
+  beforeEach(() => {
+    mockConsoleLog.mockReset();
+    mockConsoleError.mockReset();
+  });
+
+  it('should handle an invalid sync byte and errors', () => {
     const packetIndex = 0;
     const pids = [0x1122];
 
-    const mockExit = vi.spyOn(process, 'exit').mockImplementation((number) => {
-      throw new Error('process.exit: ' + number);
-    });
     handleChunk(invalidMockInputData, pids, packetIndex);
 
+    // mock process exit and error
+
+    expect(mockConsoleError).toBeCalledWith('Error: No sync byte present in packet 0, offset 0');
     expect(pids).toEqual([0x1122]); //remain unchanged
-    console.log(mockExit.mock.calls);
     expect(mockExit).toHaveBeenCalledWith(1);
   });
 });
